c6dba1f3fd01b70d343f3b70018990723c973c95
Tapas Hazra <37662903+tapashazra@users.noreply.github.com>
2023-03-19 08:17:31 -0800

    Initialize release-0.3.0

---
+++ azure-devops/azext_devops/dev/pipelines/commands.py
@@ -0,0 +1,79 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from azure.cli.core.commands import CliCommandType
+from azext_devops.dev.common.exception_handler import azure_devops_exception_handler
+from ._format import (transform_build_table_output,
+                      transform_builds_table_output,
+                      transform_build_tags_output,
+                      transform_definition_table_output,
+                      transform_definitions_table_output,
+                      transform_tasks_table_output,
+                      transform_task_table_output,
+                      transform_releases_table_output,
+                      transform_release_table_output,
+                      transform_release_definitions_table_output,
+                      transform_release_definition_table_output)
+
+
+buildOps = CliCommandType(
+    operations_tmpl='azext_devops.dev.pipelines.build#{}',
+    exception_handler=azure_devops_exception_handler
+)
+
+buildDefOps = CliCommandType(
+    operations_tmpl='azext_devops.dev.pipelines.build_definition#{}',
+    exception_handler=azure_devops_exception_handler
+)
+
+buildTaskOps = CliCommandType(
+    operations_tmpl='azext_devops.dev.pipelines.task#{}',
+    exception_handler=azure_devops_exception_handler
+)
+
+releaseOps = CliCommandType(
+    operations_tmpl='azext_devops.dev.pipelines.release#{}',
+    exception_handler=azure_devops_exception_handler
+)
+
+releaseDefinitionOps = CliCommandType(
+    operations_tmpl='azext_devops.dev.pipelines.release_definition#{}',
+    exception_handler=azure_devops_exception_handler
+)
+
+
+def load_build_commands(self, _):
+    with self.command_group('pipelines build', command_type=buildOps) as g:
+        # basic build commands
+        g.command('list', 'build_list', table_transformer=transform_builds_table_output)
+        g.command('queue', 'build_queue', table_transformer=transform_build_table_output)
+        g.command('show', 'build_show', table_transformer=transform_build_table_output)
+
+    with self.command_group('pipelines build tag', command_type=buildOps) as g:
+        # basic build tag commands
+        g.command('list', 'get_build_tags', table_transformer=transform_build_tags_output)
+        g.command('add', 'add_build_tags', table_transformer=transform_build_tags_output)
+        g.command('delete', 'delete_build_tag', table_transformer=transform_build_tags_output)
+
+    with self.command_group('pipelines build definition', command_type=buildDefOps) as g:
+        # basic build definition commands
+        g.command('list', 'build_definition_list', table_transformer=transform_definitions_table_output)
+        g.command('show', 'build_definition_show', table_transformer=transform_definition_table_output)
+
+    with self.command_group('pipelines build task', command_type=buildTaskOps) as g:
+        # basic build task commands
+        g.command('list', 'task_list', table_transformer=transform_tasks_table_output)
+        g.command('show', 'task_show', table_transformer=transform_task_table_output)
+
+    with self.command_group('pipelines release', command_type=releaseOps) as g:
+        # basic release commands
+        g.command('list', 'release_list', table_transformer=transform_releases_table_output)
+        g.command('create', 'release_create', table_transformer=transform_release_table_output)
+        g.command('show', 'release_show', table_transformer=transform_release_table_output)
+
+    with self.command_group('pipelines release definition', command_type=releaseDefinitionOps) as g:
+        # basic release commands
+        g.command('list', 'release_definition_list', table_transformer=transform_release_definitions_table_output)
+        g.command('show', 'release_definition_show', table_transformer=transform_release_definition_table_output)
---
+++ azure-devops/azext_devops/dev/team/arguments.py
@@ -0,0 +1,66 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+
+from knack.arguments import enum_choice_list
+from .const import (SERVICE_ENDPOINT_AUTHORIZATION_PERSONAL_ACCESS_TOKEN,
+                    SERVICE_ENDPOINT_TYPE_GITHUB,
+                    SERVICE_ENDPOINT_AUTHORIZATION_SERVICE_PRINCIPAL,
+                    SERVICE_ENDPOINT_TYPE_AZURE_RM)
+
+
+# CUSTOM CHOICE LISTS
+_YES_NO_SWITCH_VALUES = ['yes', 'no']
+_SOURCE_CONTROL_VALUES = ['git', 'tfvc']
+_PROJECT_VISIBILITY_VALUES = ['private', 'public']
+_STATE_VALUES = ['invalid', 'unchanged', 'all', 'new', 'wellformed', 'deleting', 'createpending']
+_SERVICE_ENDPOINT_TYPE = [SERVICE_ENDPOINT_TYPE_GITHUB, SERVICE_ENDPOINT_TYPE_AZURE_RM]
+_SERVICE_ENDPOINT_AUTHORIZATION_SCHEME = [SERVICE_ENDPOINT_AUTHORIZATION_PERSONAL_ACCESS_TOKEN,
+                                          SERVICE_ENDPOINT_AUTHORIZATION_SERVICE_PRINCIPAL]
+
+
+def load_global_args(context):
+    from azure.cli.core.commands.parameters import get_enum_type
+    context.argument('organization', options_list=('--organization', '--org'),
+                     help='Azure Devops organization URL. Example: https://dev.azure.com/MyOrganizationName/')
+    context.argument('detect', arg_type=get_enum_type(['on', 'off']),
+                     help='Automatically detect organization. Default is "on".')
+    context.argument('project', options_list=('--project', '-p'), help='Name or ID of the project.')
+
+
+def load_team_arguments(self, _):
+    with self.argument_context('devops configure') as context:
+        context.argument('defaults', options_list=('--defaults', '-d'), nargs='*')
+    with self.argument_context('devops project') as context:
+        context.argument('process', options_list=('--process', '-p'))
+        context.argument('source_control', options_list=('--source-control', '-s'),
+                         **enum_choice_list(_SOURCE_CONTROL_VALUES))
+        context.argument('description', options_list=('--description', '-d'))
+        context.argument('state', **enum_choice_list(_STATE_VALUES))
+        context.argument('visibility', **enum_choice_list(_PROJECT_VISIBILITY_VALUES))
+    with self.argument_context('devops service-endpoint create') as context:
+        context.argument('service_endpoint_type', **enum_choice_list(_SERVICE_ENDPOINT_TYPE))
+        context.argument('authorization_scheme', **enum_choice_list(_SERVICE_ENDPOINT_AUTHORIZATION_SCHEME))
+    with self.argument_context('devops project delete') as context:
+        context.argument('yes', options_list=['--yes', '-y'], action='store_true',
+                         help='Do not prompt for confirmation.')
+    with self.argument_context('devops configure') as context:
+        context.argument('use_git_aliases', **enum_choice_list(_YES_NO_SWITCH_VALUES))
+        context.argument('list_config', options_list=('--list', '-l'))
+
+    with self.argument_context('devops') as context:
+        load_global_args(context)
+
+    with self.argument_context('repos') as context:
+        load_global_args(context)
+
+    with self.argument_context('artifacts') as context:
+        load_global_args(context)
+
+    with self.argument_context('boards') as context:
+        load_global_args(context)
+
+    with self.argument_context('pipelines') as context:
+        load_global_args(context)
